9e2827a01606fb5ce134878b3135598cabcb4725
Michele Vannucci <michele.vannucci20@gmail.com>
2025-02-04 00:22:35 +0100

    finished experiments

--- PCMCI_RNN/data_manager.py
+++ PCMCI_RNN/data_manager.py
@@ -114,8 +114,7 @@ class TimeSeriesDataManager:
         # Use torch.unfold to extract sliding windows along the time dimension.
         # This creates a tensor of shape: (num_timesteps - lookback + 1, num_features, lookback)
         X_full = predictors.unfold(dimension=0, size=self.lookback, step=1)
-        print("Shape of X: ")
-        print (X_full.shape)
+        
         X_full = X_full.permute(0, 2, 1)# This creates a tensor of shape: (num_timesteps - lookback + 1, lookback, num_features)
 
         # We need exactly (num_timesteps - lookback) windows so that each window has a corresponding target.
